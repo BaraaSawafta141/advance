358584aef92af1d23824a0e9f1c6615bfb34a615
yaser906 <yaserkharouf4@gmail.com>
2024-03-22 19:26:55 +0200

    weatherapi

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 // Import statements
-import showcaseSharingRoutes from "./routes/finished_project.js"
-import collaborativePlanningRoutes from "./routes/planning.js"
 import express from "express";
 import cors from "cors";
 import user from "./routes/user.js"
-import projectRoutes from "./routes/project.js"
 import taskRoutes from "./routes/task.js"
+import collaborativePlanningRoutes from "./routes/planning.js"
+import projectRoutes from "./routes/project.js"
 import resourceRoutes from "./routes/resources.js"
+import showcaseSharingRoutes from "./routes/finished_project.js"
+import weather from "./routes/weatherapi.js"
+import setupSwagger from './routes/swagger.js';
+
 
 // Create an Express app
 const app = express();
@@ -19,11 +22,17 @@ app.use(express.json());
 // Routes
 app.use("/user",user);
 app.use("/project",projectRoutes);
+app.use("/planning",collaborativePlanningRoutes);
 app.use("/task",taskRoutes);
 app.use("/resources",resourceRoutes);
 app.use("/finished",showcaseSharingRoutes);
-app.use("/planning",collaborativePlanningRoutes);
+app.use("/weather",weather);
+// app.use("/swagger",setupSwagger);
 // Start the Express server
 app.listen(3000, () => {
   console.log("server Starting at http://localhost:3000");
 });
+
+
+
+
